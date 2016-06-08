a3a2f1d23e31965b6a5a2807aa99e75cfb6845d3
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-08 17:58:47 -0500

    squelch link script output

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -1,1 +1,1 @@
-"%PREFIX%\Scripts\jupyter-nbextension.exe" enable ipyleaflet --py --sys-prefix && if errorlevel 1 exit 1
+"%PREFIX%\Scripts\jupyter-nbextension.exe" enable ipyleaflet --py --sys-prefix > NUL 2>&1 && if errorlevel 1 exit 1
--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,1 +1,1 @@
-"${PREFIX}/bin/jupyter-nbextension" enable ipyleaflet --py --sys-prefix
+"${PREFIX}/bin/jupyter-nbextension" enable ipyleaflet --py --sys-prefix > /dev/null 2>&1
