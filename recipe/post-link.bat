c72a43aaa810c06dc4e39fc13822a7c681ca91a6
martinRenou <martin.renou@gmail.com>
2018-12-13 17:43:17 +0100

    Remove post-link files

--- recipe/post-link.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-(
-  "%PREFIX%\Scripts\jupyter-nbextension.exe" enable ipyleaflet --py --sys-prefix
-) >>"%PREFIX%\.messages.txt" 2>&1
--- recipe/post-link.sh
+++
@@ -1,3 +0,0 @@
-{
-  "${PREFIX}/bin/jupyter-nbextension" enable ipyleaflet --py --sys-prefix 
-} >> "${PREFIX}/.messages.txt" 2>&1
